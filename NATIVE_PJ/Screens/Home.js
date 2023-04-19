4156f126046e2f74b5ebf05def778ae742612243
Nataliia Svystula <105055646+Nataliia0202@users.noreply.github.com>
2023-04-19 22:41:27 +0300

    add svg

---
+++ NATIVE_PJ/Screens/Home.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect, useCallback } from "react";
+import SVGgrid from "../assets/images/grid.svg";
+import SVGlogout from "../assets/images/log-out.svg";
+import SVGuser from "../assets/images/user.svg";
+import SVGunion from "../assets/images/Union.svg";
