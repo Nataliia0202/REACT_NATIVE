4529e24a072f424066e1cb46a7e857111c23c0e9
Nataliia Svystula <105055646+Nataliia0202@users.noreply.github.com>
2023-04-25 22:59:39 +0300

    add

--- NATIVE_PJ/Screens/CreatePostsScreen.js
+++ NATIVE_PJ/Screens/CreatePostsScreen.js
@@ -1,21 +1,335 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Image,
+  Pressable,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  ScrollView,
+  Alert,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import * as Location from "expo-location";
+import { storage, db } from "../firebase/config";
+import { v4 } from "uuid";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { addDoc, collection } from "firebase/firestore";
+import { useSelector } from "react-redux";
+import { selectID, selectName } from "../redux/selectors";
+import { useIsFocused } from "@react-navigation/native";
+import * as ImagePicker from "expo-image-picker";
 
-export const CreatePostsScreen = () => {
-    return (
-    <>
-        <View style={styles.container}>
-            <Text>CreatePostsScreen</Text>
-        </View>
-    </>
-  )
+export const CreatePostsScreen = ({ navigation }) => {
+const [imageSignature, setImageSignature] = useState("");
+const [imageLocation, setImageLocation] = useState("");
+const [hasPermission, setHasPermission] = useState(null);
+const [cameraRef, setCameraRef] = useState(null);
+const [type, setType] = useState(Camera.Constants.Type.back);
+const [photo, setPhoto] = useState(null);
+const [location, setLocation] = useState(null);
+const [errorMsg, setErrorMsg] = useState(null);
+const uid = useSelector(selectID);
+const name = useSelector(selectName);
+  const isFocused = useIsFocused();
   
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        setLocation({
+          coords: {
+            latitude: 50.011206,
+            longitude: 36.241585,
+          },
+        });
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  const imageTitleHandler = (text) => {
+    setImageSignature(text);
+  };
+
+
+  const uploadPhotoToStorage = async (uri) => {
+    try {
+      const response = await fetch(uri);
+          const file = await response.blob();
+          const imageId = v4();
+
+          const storageRef = ref(storage, `postImage/${imageId}`);
+          await uploadBytes(storageRef, file);
+          const storageUrlPhoto = await getDownloadURL(
+            ref(storage, `postImage/${imageId}`)
+          );
+          console.log(storageUrlPhoto);
+          return storageUrlPhoto;
+    } catch (error) {
+      Alert.alert("Try again \n", error.message);
+    }
+    
+  };
+
+
+  const uploadPostToStorage = async () => {
+    const photo = await uploadPhotoToStorage();
+    console.log(imageSignature, imageLocation, location, photo);
+    try {
+      const valueObj = {
+        name,
+        uid,
+        imageSignature,
+        imageLocation,
+        photo,
+        commentCounter: 0,
+      };
+      if (location) valueObj.location = location.coords;
+      const docRef = await addDoc(collection(db, "posts"), valueObj);
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const imageLocationHandler = (text) => {
+    setImageLocation(text);
+  };
+
+  const imageDownloaderHandler = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImageSignature(await uploadPhotoToStorage(result.assets[0].uri));
+    }
+    console.log("upload");
+  };
+  const onSubmit = async () => {
+    await uploadPostToStorage();
+    setImageSignature("");
+    setImageLocation("");
+    setPhoto(null);
+    navigation.navigate("Публикации");
+  };
+
+  const flipCamera = () => {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  };
+
+  const takePhoto = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync();
+      setPhoto(photo.uri);
+    }
+  };
+ 
+
+console.log("CreatePostsScreen");
+
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ScrollView styles={styles.box} backGroundColor="#fff">
+          <View style={styles.container}>
+            {isFocused && (
+              <Camera
+                style={styles.camera}
+                type={type}
+                ref={(ref) => {
+                  setCameraRef(ref);
+                }}
+              >
+                {photo && (
+                  <View style={styles.takePhotoContainer}>
+                    <Image
+                      source={{ uri: photo }}
+                      style={{ width: 200, height: 200 }}
+                    />
+                  </View>
+                )}
+                <TouchableOpacity
+                  style={styles.flipContainer}
+                  onPress={flipCamera}
+                >
+                  <MaterialCommunityIcons
+                    name="camera-front-variant"
+                    size={24}
+                    style={{ color: "white" }}
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.shootButton}
+                  onPress={takePhoto}
+                >
+                  <View style={styles.takePhotoOut}>
+                    <View style={styles.takePhotoInner}>
+                      <MaterialCommunityIcons name="camera-outline" size={24} />
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              </Camera>
+            )}
+            <Pressable onPress={imageDownloaderHandler}>
+              <Text style={styles.text}>Загрузите фото</Text>
+            </Pressable>
+
+            <TextInput
+              value={imageSignature}
+              onChangeText={imageTitleHandler}
+              placeholder="Название..."
+              style={styles.input}
+            />
+            <View position="relative">
+              <Pressable style={styles.mapButton}>
+                <MaterialCommunityIcons name="map-marker-outline" size={24} color="#BDBDBD"/>
+              </Pressable>
+
+              <TextInput
+                value={imageLocation}
+                onChangeText={imageLocationHandler}
+                placeholder="Местность..."
+                style={styles.input}
+              ></TextInput>
+            </View>
+            <Pressable onPress={onSubmit} style={styles.button}>
+              <Text style={styles.buttonText}>Опубликовать</Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
+    backGroundColor: "#fff",
     flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  camera: {
+    height: 300,
+    marginTop: 30,
+    borderRadius: 5,
+  },
+  photoView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+
+  flipContainer: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    marginRight: 10,
+    marginTop: 10,
+  },
+
+  shootButton: {
+    alignSelf: "center",
+    top: 200,
+  },
+
+  takePhotoContainer: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    borderColor: "#ddd",
+    borderWidth: 3,
+    width: 200,
+    height: 200,
+  },
+
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "#ffffff67",
+    height: 50,
+    width: 50,
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 50,
+  },
+
+  takePhotoInner: {
+    borderWidth: 2,
+    borderColor: "#ccc",
+    height: 40,
+    width: 40,
+    backgroundColor: "#ffffff80",
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  image: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: 343,
+    height: 240,
+  },
+
+  text: {
+    marginTop: 10,
+    color: "#BDBDBD",
+  },
+
+  input: {
+    paddingLeft: 50,
+    height: 44,
+    padding: 10,
+    backgroundColor: "#E8E8E8",
+    marginTop: 16,
+    borderRadius: 10,
+  },
+  mapButton: {
+    position: "absolute",
+    top: 26,
+    left: 15,
+    zIndex: 2,
+  },
+  button: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 32,
+    padding: 16,
+    marginVertical: 16,
+    marginTop: 43,
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "white",
+    textAlign: "center",
   },
 });
--- NATIVE_PJ/Screens/Home.js
+++ NATIVE_PJ/Screens/Home.js
@@ -7,69 +7,81 @@ import { PostsScreen } from "./PostsScreen";
 import { CreatePostsScreen } from "./CreatePostsScreen";
 import { ProfileScreen } from "./ProfileScreen";
 import { StyleSheet, Text, View, Pressable } from "react-native";
+
+
+
 const Tab = createBottomTabNavigator();
 
 
-export const Home = () => {
-    const dispatch = useDispatch();
+export const Home = ({ navigation }) => {
+  const dispatch = useDispatch();
 
-    const logOutHandler = () => {
-      dispatch(logOut());
-    };
+  const logOutHandler = () => {
+    dispatch(logOut());
+  };
 
+  return (
+    <Tab.Navigator
+      style={{ paddingTop: 9, height: 83 }}
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-    return (
-      <Tab.Navigator
-        style={{ paddingTop: 9, height: 83 }}
-        screenOptions={({ route }) => ({
-          tabBarShowLabel: false,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+          if (route.name === "Публикации") {
+            iconName = "view-grid-outline";
+          } else if (route.name === "Создать публикацию") {
+            iconName = "plus";
+          } else if (route.name === "Профиль") {
+            iconName = "account-outline";
+          }
 
-            if (route.name === "Публикации") {
-              iconName = "view-grid-outline";
-            } else if (route.name === "Создать публикацию") {
-              iconName = "plus";
-            } else if (route.name === "Профиль") {
-              iconName = "account-outline";
-            }
-
-            return (
-              <View
-                style={{
-                  width: 70,
-                  height: 40,
-                  borderRadius: 20,
-                  backgroundColor: focused ? "#FF6C00" : "#fff",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <MaterialCommunityIcons
-                  name={iconName}
-                  color={color}
-                  size={24}
-                />
-              </View>
-            );
-          },
-          tabBarActiveTintColor: "#fff",
-          tabBarInactiveTintColor: "gray",
-        })}
-      >
-        <Tab.Screen
-          name="Публикации"
-          component={PostsScreen}
-          options={{
-            headerRight: () => (
-              <Pressable onPress={logOutHandler}>
-                <MaterialCommunityIcons name="logout" size={24} color="black" />
-              </Pressable>
-            ),
-          }}
-        />
-        <Tab.Screen name="Создать публикацию" component={CreatePostsScreen} />
-        <Tab.Screen name="Профиль" component={ProfileScreen} />
-      </Tab.Navigator>
-    );
-}
+          return (
+            <View
+              style={{
+                width: 70,
+                height: 40,
+                borderRadius: 20,
+                backgroundColor: focused ? "#FF6C00" : "#fff",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <MaterialCommunityIcons name={iconName} color={color} size={24} />
+            </View>
+          );
+        },
+        tabBarActiveTintColor: "#fff",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen
+        name="Публикации"
+        component={PostsScreen}
+        options={{
+          headerRight: () => (
+            <Pressable onPress={logOutHandler}>
+              <MaterialCommunityIcons name="logout" size={24} color="black" />
+            </Pressable>
+          ),
+        }}
+      />
+      <Tab.Screen
+        options={{
+          headerRight: () => (
+            <Pressable onPress={() => navigation.navigate("Публикации")}>
+              <MaterialCommunityIcons
+                name="arrow-left"
+                size={24}
+                color="black"
+              />
+            </Pressable>
+          ),
+        }}
+        name="Создать публикацию"
+        component={CreatePostsScreen}
+      />
+      <Tab.Screen name="Профиль" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
